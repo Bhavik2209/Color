9661d5948e58ff92af5d0f651cef2505284b14bb
Bhavik2209 <117085679+Bhavik2209@users.noreply.github.com>
2023-07-22 17:30:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let Blue=document.getElementById("blue")
+let yellow=document.getElementById("yellow")
+let red=document.getElementById("red")
+let green=document.getElementById("green")
+let body=document.querySelector("body")
+function colorChange1(){
+    body.style.backgroundColor="blue"
+} 
+function colorChange2(){
+    body.style.backgroundColor="green"
+}function colorChange3(){
+    body.style.backgroundColor="yellow"
+}function colorChange4(){
+    body.style.backgroundColor="red"
+}
